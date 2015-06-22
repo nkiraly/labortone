0258803026f7db434f3dc530b24d01c6f4396511
Nicholas Kiraly <kiraly.nicholas@gmail.com>
2015-06-21 22:59:45 -0400

    add junit data provider support and use it to do all permutations of task ordering for dependency sort unit testing

--- src/test/java/com/nicholaskiraly/research/taskplanner/PlanSolverBruteForceTest.java
+++ src/test/java/com/nicholaskiraly/research/taskplanner/PlanSolverBruteForceTest.java
@@ -1,5 +1,8 @@
 package com.nicholaskiraly.research.taskplanner;
 
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -15,9 +18,11 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 import org.yaml.snakeyaml.Yaml;
 
+@RunWith(DataProviderRunner.class)
 public class PlanSolverBruteForceTest {
 
   public PlanSolverBruteForceTest() {
@@ -40,8 +45,8 @@ public class PlanSolverBruteForceTest {
   }
 
   @Test
-  public void testCalculateTaskDepends002() throws FileNotFoundException, IOException {
-    String taskFileName = "/tasks002_unsorted.yml";
+  @UseDataProvider("provideTaskFileResources")
+  public void testCalculateTaskDepends(String taskFileName) throws FileNotFoundException, IOException {
     assertNotNull("Tasks resource " + taskFileName + " not found on class path", getClass().getResource(taskFileName));
     File taskFile = new File(getClass().getResource(taskFileName).getFile());
     assertTrue("Tasks file " + taskFileName + " does not exist", taskFile.exists());
@@ -67,32 +72,21 @@ public class PlanSolverBruteForceTest {
     assertEquals("Third task is not task3", "task3", sortedKeyArray[2]);
   }
   
-  @Test
-  public void testCalculateTaskDepends003() throws FileNotFoundException, IOException {
-    String taskFileName = "/tasks003_unsorted.yml";
-    assertNotNull("Tasks resource " + taskFileName + " not found on class path", getClass().getResource(taskFileName));
-    File taskFile = new File(getClass().getResource(taskFileName).getFile());
-    assertTrue("Tasks file " + taskFileName + " does not exist", taskFile.exists());
-
-    FileInputStream taskFIS = new FileInputStream(taskFile);
-    Yaml yaml = new Yaml();
-    String taskContent = IOUtils.toString(taskFIS);
-    Map unsortedTaskMap = (LinkedHashMap) yaml.load(taskContent);
-
-    assertNotNull("Unsorted Task Map is null", unsortedTaskMap);
-    assertNotEquals("Unsorted Task file " + taskFileName + " load resulted in 0 tasks in list", unsortedTaskMap.size(), 0);
-
-    PlanSolverBruteForce psbf = new PlanSolverBruteForce();
-
-    Map sortedTaskMap = psbf.calculateTaskDepends(unsortedTaskMap);
-
-    // confirm task dependency order
-    Set sortedTaskKeySet = sortedTaskMap.keySet();
-    assertEquals("Sorted Task Key Set is not size 3", 3, sortedTaskKeySet.size());
-    String[] sortedKeyArray = (String[]) sortedTaskKeySet.toArray(new String[sortedTaskKeySet.size()]);
-    assertEquals("First task is not task1", "task1", sortedKeyArray[0]);
-    assertEquals("Second task is not task2", "task2", sortedKeyArray[1]);
-    assertEquals("Third task is not task3", "task3", sortedKeyArray[2]);
+  /**
+   * Tasks YAML file resource name Provider
+   * 
+   * for more info, see https://github.com/TNG/junit-dataprovider/wiki/Getting-started#usage
+   * 
+   * @return 
+   */
+  @DataProvider
+  public static String[] provideTaskFileResources() {
+    return new String[]{
+      "/tasks002_unsorted.yml",
+      "/tasks003_unsorted.yml",
+      "/tasks004_unsorted.yml",
+      "/tasks005_unsorted.yml",
+    };
   }
 
 }
